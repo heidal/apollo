6be9a6c23d6efdd3f850d8b30d6b38ee6d52b50f
Adam Budziak <adambudziak@protonmail.com>
2020-01-14 11:47:05 +0100

    add django-debug-toolbar

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,11 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = []
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))
+    ]
+
+
+urlpatterns += [
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
