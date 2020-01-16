d327c53f4683d8fbd0481cd6bcb3f447181fc1b3
Adam Budziak <52408517+adambudziak@users.noreply.github.com>
2020-01-16 09:39:10 +0100

    Add pytest (#4)
    * add django-rest-framework
    
    * move to runserver_plus for local development, remove static files
    
    * add pytest and setup tests
    
    * run black

--- backend/config/settings/local.py
+++ backend/config/settings/local.py
@@ -4,13 +4,9 @@ from .base import *
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-INSTALLED_APPS += [
-    'debug_toolbar'
-]
+INSTALLED_APPS += ["debug_toolbar"]
 
-MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
-] + MIDDLEWARE
+MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
 ip = socket.gethostbyname(socket.gethostname())
 INTERNAL_IPS += [ip[:-1] + "1"]
--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -2,8 +2,20 @@ from .base import *
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
+# without DEBUG tests run faster
+DEBUG = False
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
+    }
+}
+
+TEST_RUNNER = "config.runner.PytestTestRunner"
