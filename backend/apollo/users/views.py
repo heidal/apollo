c6bcfff5d9ea014dc03d2b95066ef3263913f291
Adam Budziak <52408517+adambudziak@users.noreply.github.com>
2020-03-19 08:10:58 +0100

    add vue frontend and implement logging in/out and registration flow (#14)

--- backend/apollo/users/views.py
+++ backend/apollo/users/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from apollo.users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     # for showcase purposes
     queryset = User.objects.all()
     serializer_class = UserSerializer
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -30,8 +30,8 @@ if settings.DEBUG:
 
 apipatterns += [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("", include("social_django.urls", namespace="social")),
+    path("rest-auth/", include("rest_auth.urls")),
+    path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 apipatterns += users_urls.urlpatterns
