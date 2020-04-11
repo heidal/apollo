d3b2815e29b66de551cb37ea8c52ccbe0255588c
Konrad Swierczynski <konrad.swierczynski@nokia.com>
2020-04-11 23:14:19 +0200

    Add openapi schema endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
 
 apipatterns = []
 
@@ -17,6 +18,13 @@ apipatterns += [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("users/", include("apollo.users.urls", namespace="users")),
     path("elections/", include("apollo.elections.urls", namespace="elections")),
+    path(
+        "openapi",
+        get_schema_view(
+            title="Apollo", description="API in Apollo Voting", version="1.0.0"
+        ),
+        name="openapi-schema",
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [path("api/", include(apipatterns))]
