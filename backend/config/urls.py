60b0de33a22d93f38fe4cc8e248ce6cefbe20e5d
Konrad Swierczynski <konrad.swierczynski@nokia.com>
2020-04-13 20:00:23 +0200

    Add swagger and redoc ui endpoints

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 
 apipatterns = []
@@ -19,11 +20,29 @@ apipatterns += [
     path("users/", include("apollo.users.urls", namespace="users")),
     path("elections/", include("apollo.elections.urls", namespace="elections")),
     path(
-        "openapi",
+        "openapi/",
         get_schema_view(
-            title="Apollo", description="API in Apollo Voting", version="1.0.0"
+            title="Apollo",
+            description="OpenAPI documentation for Apollo Voting",
+            version="1.0.0",
         ),
         name="openapi-schema",
+    ),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
+    path(
+        "redoc/",
+        TemplateView.as_view(
+            template_name="redoc-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="redoc",
     ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
