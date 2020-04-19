ba0c4c01fd6f54945d00e6f5d4bc74c745a316f8
Konrad Świerczyński <konrad.swie@gmail.com>
2020-04-19 11:22:59 +0200

    Add OpenAPI schema (#23)
    * Add openAPI schema endpoint
    * Add swagger and redoc ui endpoints
    
    Co-authored-by: Konrad Swierczynski <konrad.swierczynski@nokia.com>

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 apipatterns = []
 
@@ -17,6 +19,31 @@ apipatterns += [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("users/", include("apollo.users.urls", namespace="users")),
     path("elections/", include("apollo.elections.urls", namespace="elections")),
+    path(
+        "openapi/",
+        get_schema_view(
+            title="Apollo",
+            description="OpenAPI documentation for Apollo Voting",
+            version="1.0.0",
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        "swagger-ui/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="swagger-ui",
+    ),
+    path(
+        "redoc/",
+        TemplateView.as_view(
+            template_name="redoc-ui.html",
+            extra_context={"schema_url": "openapi-schema"},
+        ),
+        name="redoc",
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [path("api/", include(apipatterns))]
