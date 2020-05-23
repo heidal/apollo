20b4dff25713a3605615b2fa9312ad35c0628bd7
Konrad Swierczynski <konrad.swierczynski@nokia.com>
2020-05-23 21:20:23 +0200

    Change vote model

--- backend/apollo/elections/models/vote.py
+++ backend/apollo/elections/models/vote.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.db.models import UniqueConstraint
 
 
 class Vote(models.Model):
-    answer = models.ForeignKey("Answer", on_delete=models.CASCADE, related_name="votes")
+    answer = models.ForeignKey(
+        "Answer", on_delete=models.CASCADE, blank=True, null=True
+    )
+    answer_ciphertext = models.CharField(max_length=400)
+    question = models.ForeignKey(
+        "Question", on_delete=models.CASCADE, related_name="votes"
+    )
     author = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="votes"
     )
     created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        constraints = [
-            UniqueConstraint(fields=["answer", "author"], name="unique_answer_author")
-        ]
